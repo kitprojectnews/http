c177ebe4f0c96c4807df58caf14ac0798cbf3569
BeomJinKim <project@localhost.localdomain>
2019-07-24 16:42:19 +0900

    logview noCSS ver

---
+++ php/logview.php
@@ -0,0 +1,23 @@
+<html>
+    <title>LogView</title>
+    <body>
+        <form>
+            <table border=1>
+                <tr>
+                    <th>Time</th>
+                    <th>Action</th>
+                </tr>
+                <?php
+                    echo ('<tr><td>');
+                    $all = shell_exec('grep Observer /var/log/messages');
+                    $line = str_replace(PHP_EOL, '</tr><tr><td>', $all);
+                    $tmp1 = preg_replace("/localhost \[Observer\]\[[0-9]{3,6}\]: /", "", $line);
+                    $tmp2 = preg_replace("/ \[/","<td>[",$tmp1);
+                    $tmp3 = substr($tmp2,0,-8);
+                    echo $tmp3;
+                ?>
+            </table>
+        </form>
+    </body>
+</html>
+
