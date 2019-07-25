c9b53a9ae868e731b83f2668199537efae2bcd09
StarRate1214 <kit@localhost.localdomain>
2019-07-25 16:52:46 +0900

    세션 추가

---
+++ input.php
@@ -0,0 +1,34 @@
+<?php
+// session_start();
+// if (!isset($_SESSION['user_num'])) 
+// {
+//     header('Location:./index.html');
+// }
+
+
+$user = "qwer1234";
+$pass = "qwer1234";
+
+$hostName='localhost';
+$dbuserName='msr';
+$passWord='Qwer!234';
+$dbName='test';
+
+// Create connection
+$conn = mysqli_connect($hostName, $dbuserName, $passWord, $dbName);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+$hashpw = base64_encode(hash('sha256', $pass, true));
+echo $hashpw."<br>";
+$sql = "INSERT INTO test.account (`u_id`, `u_pw`, `u_active`, `u_update`, `r_sel`, `r_update`) VALUES ( '".$user."','".$hashpw."', 1, 1, 1, 1)";
+if ($conn->query($sql) === TRUE) {
+    echo "ok<br>";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
