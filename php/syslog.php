e960b5833e9c5ff36f3a34b34d136e5ddd4021b0
BeomJinKim <project@localhost.localdomain>
2019-07-25 13:33:38 +0900

    syslog fix

--- php/syslog.php
+++ php/syslog.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <link rel="stylesheet" type="text/css" href="css/Observer_syslog.css" />
+        <link rel="stylesheet" type="text/css" href="../css/Observer_syslog.css" />
     </head>
     <title>SYSLOG</title>
     <body>
@@ -15,7 +15,7 @@
                 <tbody>
                     <?php
                         echo ('<tr><th scope="row">');
-                        $all = shell_exec('grep Observer /var/log/messages');
+                        $all = shell_exec('grep Observer /var/log/messages | sort -r');
                         $line = str_replace(PHP_EOL, '</td></tr><tr><th scope="row">', $all);
                         $tmp1 = preg_replace("/localhost \[Observer\]\[[0-9]{3,6}\]: /", "", $line);
                         $tmp2 = preg_replace("/ \[/","</th><td>[",$tmp1);
