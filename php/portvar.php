54831aead9d126c7b64c2d6b3c1ea8a4e333ec47
CielTMP <49310205+CielTMP@users.noreply.github.com>
2019-07-29 10:43:22 +0900

    Update portvar.php

--- php/portvar.php
+++ php/portvar.php
@@ -1,6 +1,73 @@
 <?php
     include 'dbconn.php';
+    $vname = $_POST["vname"];
+    if($vname){
+        $sql="select v_value from sig_port_variables where v_name='".$vname."';";
+        $result = $conn->query($sql);
+		$row = $result->fetch_assoc();
+		$port = explode(':', $row["v_value"]);
+    }
 ?>
+
+<script src="rmain2.js"></script>
+<form method=post 
+    <?php 
+        if($vname == "")
+        { ?> 
+            action="addport_insert.php" <?php 
+        } 
+        else
+        { ?>
+            action="addport_update.php" <?php 
+        } 
+    ?>
+>
+<script src="rmain2.js"></script>
+<form method=post 
+    <?php 
+        if($vname == "")
+        { ?> 
+            action="addport_insert.php" <?php 
+        } 
+        else
+        { ?>
+            action="addport_update.php" <?php 
+        } 
+    ?>
+>
+<h1>ADD Port Variable</h1>
+<table border=1>
+<tr>
+    <td>NAME</td>
+    <td><input type="text" name="portname" <?php if($vname != "") { ?> readonly <?php } ?> value="<?=substr($vname, 1)?>" ></td>
+    </tr>
+    </tr>
+    <td>VALUE</td>
+    <td>
+        <input type="text" size=3 id="var_value_port1" name="port1" 
+		onkeydown="return onlyNumber(event)" 
+		onkeyup="removeChar(event)" 
+		onfocusout="removeChar(event)" 
+		oninput="portRange(this)" 
+		style="ime-mode:disabled" value=<?=$port[0]?>> ~ 
+		<input type="text" size=3 id="var_value_port2" name="port2" 
+		onkeydown="return onlyNumber(event)" 
+		onkeyup="removeChar(event)" 
+		onfocusout="removeChar(event)" 
+		oninput="portRange(this)" 
+		style="ime-mode:disabled" value=<?=$port[1]?>>
+    </td>
+</tr>
+<tr>
+</tr>
+</table>
+<?php if($vname != "") { ?>
+        <input type="submit" value="수정하기" >  
+        <?php } else { ?>
+        <input type="submit" value="추가하기" >
+        <?php } ?>
+
+</form>
 <h2 align=center>Port Variable List</h2>
 <a href="addport.php" align=center>Port 변수 추가</a>
 <table border=1 align=center>
