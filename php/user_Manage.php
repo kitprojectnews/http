66ce1365c32ccc4701167f8e7ad6c4068c32c997
StarRate1214 <kit@localhost.localdomain>
2019-07-29 15:11:35 +0900

    유저관리 modi del 추가 && table용 css 추가

--- php/user_Manage.php
+++ php/user_Manage.php
@@ -3,8 +3,6 @@
 <head>
 <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
-
-<!-- <link rel="stylesheet" type="text/css" href="../css/Observer_Login.css"> -->
 <link rel="stylesheet" type="text/css" href="../css/Observer_user_Manage.css">
 <?php
 session_start();
@@ -21,44 +19,46 @@ session_start();
 //     echo " <script>alert('접근권한이 없습니다. '); history.back(); </script>";
 // }
 ?>
-    <title></title>
+    <title>유저 관리 페이지 </title>
 </head>
 <body>
 <form>
 <table width=100%>
-<tr><th>관리번호</th><td>ID</td><td>활성여부</td><td>유저 수정권한</td><td>룰 확인</td><td>룰 추가/수정/삭제</td><td>수정 및 삭제</td></tr>
+<tr><th>관리번호</th><th>ID</th><th>활성여부</th><th>유저 관리권한</th><th>룰 확인</th><th>룰 추가/수정/삭제</th><th>수정 및 삭제</th></tr>
 <?php
-include "./dbcdonn.php";
-$sql = "SELECT u_num, u_id, u_active, u_update, r_sel, r_update FROM test.account ORDER BY DESC";
+include "dbconn.php";
+$sql = "SELECT u_num, u_id, u_active, u_update, r_sel, r_update FROM account ORDER BY u_num DESC";
 $result = $conn->query($sql);
-echo "ok<br>";
-if ($result->num_rows != 0) {
-    while($row = $result->fetch_assoc()) 
-    {
-        echo ("<tr>");
-        echo ("<th scope='row'>" . $row["u_num"] . "</th>");
-        echo ("<td>" . $row["u_id"] . "</td>");
-        echo ("<td>" . $row["u_active"] . "</td>");
-        echo ("<td>" . $row["r_sel"]. "</td>");
-        echo ("<td>" . $row["r_update"] . "</td>");
-        echo ("<td> <button onClick='window.open(\"user_Modi.php\",\"user Modifiy\",\"width=10\",\"height=10\")'>수정</button>&nbsp;<button/>삭제</td>");
-        echo ("</tr>");
-        $row["u_num"];
-        $row["u_id"];
-        $row["u_active"];
-        $row["u_update"];
-        $row["r_sel"];
-        $row["r_update"];
-        $conn->close();
-    }
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {?>
+        <tr>
+        <th scope='row'><?=$row["u_num"]?></th>
+        <td><?=$row["u_id"]?></td>
+        <td><?=$row["u_active"]?></td>
+        <td><?=$row["u_update"]?></td>
+        <td><?=$row["r_sel"]?></td>
+        <td><?=$row["r_update"]?></td>
+        <td><input type=button value="수정" onClick='userhref("<?=$row["u_num"]?>")'>&nbsp;<input type=submit value=삭제 onClick='delhref("<?=$row["u_num"]?>")'></td>
+    </tr><?php
+        }
+} else {
+    echo "0 results<br>";
 }
-else {
-    $conn->close();
-}
-echo "ok<br>";
 ?>
-
 </table>
 </form>
 </body>
+<script launguage='JAVASCRIPT'>
+    //sig_msg에 하이퍼링크 걸기, 패킷뷰에 탭넣어서 ipheader들 볼수있게 TODOTODO
+    function userhref(u_num) {
+        window.open('user_Modi.php?u_num='+u_num, 'userModi', 'width = 700, height = 250, menubar = no, status = no, toolbar = no, scrollbars = no, resizable = no, location = no');
+    }
+    function delhref(u_num) {
+        a = confirm("관리번호 "+u_num+" 정말로 삭제하시겠습니까?");
+        if(a)
+        {
+            window.open('user_Del.php?u_num='+u_num, 'userModi', 'width = 0, height = 0, menubar = no, status = no, toolbar = no, scrollbars = no, resizable = no, location = no');
+        }
+    }
+    </script>
 </html>
---
+++ php/user_Modi.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<meta http-equiv="Content-Type" content="text/html; charset=EUC-KR">
+<head>
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+<link rel="stylesheet" type="text/css" href="fonts/Linearicons-Free-v1.0.0/icon-font.min.css">
+
+<!-- <link rel="stylesheet" type="text/css" href="../css/Observer_Login.css"> -->
+<link rel="stylesheet" type="text/css" href="../css/Observer_user_Manage.css">
+<?php
+session_start();
+// if (!isset($_SESSION['user_num']))//세션 확인
+// {
+//     header('Location:../index.html');
+// }
+// if(!$_SESSION['u_active'])//활성 유저 여부 
+// {
+//     echo " <script>alert('비활성화된 사용자입니다. '); history.back(); </script>";
+// }
+// if(!$_SESSION['u_update'])//유저 관리권한 여부
+// {
+//     echo " <script>alert('접근권한이 없습니다. '); history.back(); </script>";
+// }
+?>
+    <title>유저 수정 페이지 </title>
+    </head>
+<body>
+<?php 
+$u_num = $_GET["u_num"];
+?>
+<form action="user_modi_ok.php" method="POST">
+<table width=70% align="center">
+<?php
+include "dbconn.php";
+
+$sql = "SELECT u_num, u_id, u_active, u_update, r_sel, r_update FROM account Where u_num ='".$u_num."'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) { ?>
+        <tr><th colspan=2>관리번호&nbsp;<?=$row["u_num"]?> <input type="hidden" name="u_num" value="<?=$row["u_num"]?>"></tr>
+        <tr><th>ID</th><td><?= $row["u_id"]?></td></tr>
+        <tr><th>PW</th><td> <input name="u_pw" type='password' value=""></td></tr>
+        <tr><th>활성여부</th><td><input name="u_active" type='checkbox' value="1" <?php if($row["u_active"] == "1") { ?> checked <?php } ?>></td></tr>
+        <tr><th>관리권한</th><td><input name="u_update" type='checkbox' <?php if($row["u_update"] == "1") { ?> checked <?php } ?>></td></tr>
+        <tr><th>룰 조회</th><td><input name="r_sel" type='checkbox' <?php if($row["r_sel"] == "1") { ?> checked <?php } ?>></td></tr>
+        <tr><th>룰 추가/수정/삭제</th><td><input name="r_update" type='checkbox' <?php if($row["r_update"] == "1") { ?> checked <?php } ?>></td></tr>
+        <tr><td colspan="2"><input type="submit" value="수정">&nbsp;&nbsp;<button onClick="<script> self.close(); </script>">취소</button> </td></tr>
+        <?php
+        }
+} else {
+    echo "0 results<br>";
+}
+?>
+</table>
+</form>
+</body>
+</html>
