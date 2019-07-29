837ab8fee3abb76c09879e17fc0fa8e51bcda58e
CielTMP <49310205+CielTMP@users.noreply.github.com>
2019-07-29 12:51:02 +0900

    Update ipvar.php

--- php/ipvar.php
+++ php/ipvar.php
@@ -1,106 +1,42 @@
 <?php
     include 'dbconn.php';
-    
-    $vname = $_POST["vname"];
-    //ip수정
-    if($vname){
-        $sql="select v_value from sig_ip_variables where v_name='".$vname."';";
-        $result = $conn->query($sql);
-		$row = $result->fetch_assoc();
-		$ip = explode('.', $row["v_value"]);
-        $sm = explode('/', $ip[3]);
-    }
 ?>
+
 <script src="rmain2.js"></script>
 <a href="addip.php" align=center>ip변수 추가</a>
-<h2 align=center>ADD</h2>
-<form method=post 
-    <?php 
-        if($vname == "")
-        { ?> 
-            action="addip_insert.php" <?php 
-        } 
-        else
-        { ?>
-            action="addip_update.php" <?php 
-        } 
-    ?>
->
-
-<table border=1 align=center>
-<tr>
-    <td align=center>NAME</td>
-    <td>
-        <input type="text" id="ipid" name="ipname" value="<?=substr($vname, 1)?>" <?php if($vname) { ?> readonly <?php } ?> >
-    </td>
-</tr>
-<tr>
-    <td align=center>VALUE</td>
-    <td>
-        <input type="text" size=3 id="var_value_ip1" name="ip1" 
-        onkeydown="return onlyNumber(event)" 
-        onkeyup="removeChar(event)" 
-        onfocusout="removeChar(event)" 
-        oninput="ipRange(this)" 
-        style="ime-mode:disabled" value=<?=$ip[0]?> > . 
-        <input type="text" size=3 id="var_value_ip2" name="ip2" 
-        onkeydown="return onlyNumber(event)" 
-        onkeyup="removeChar(event)" 
-        onfocusout="removeChar(event)" 
-        oninput="ipRange(this)" 
-        style="ime-mode:disabled" value=<?=$ip[1]?>> . 
-        <input type="text" size=3 id="var_value_ip3" name="ip3" 
-        onkeydown="return onlyNumber(event)" 
-        onkeyup="removeChar(event)" 
-        onfocusout="removeChar(event)" 
-        oninput="ipRange(this)" 
-        style="ime-mode:disabled" value=<?=$ip[2]?>> . 
-        <input type="text" size=3 id="var_value_ip4" name="ip4" 
-        onkeydown="return onlyNumber(event)" 
-        onkeyup="removeChar(event)" 
-        onfocusout="removeChar(event)" 
-        oninput="ipRange(this)" 
-        style="ime-mode:disabled" value=<?=$sm[0]?>> / 
-        <input type="text" size=3 id="var_value_sm" name="ipsm" 
-        onkeydown="return onlyNumber(event)" 
-        onkeyup="removeChar(event)" 
-        onfocusout="removeChar(event)" 
-        oninput="subnetmaskRange(this)" 
-        style="ime-mode:disabled" value=<?=$sm[1]?>>
-    </td>
-</tr>
-</table>
-<div align=center>
-	<input type="submit" value="ADD">
-</div>
-</form>
 
 <h2 align=center>LIST</h2>
+<input type="button" value="ADD">
 <table border=1 align=center>
-	<tr><td colspan=4 align=center>IP tables</tr>
-	<tr><td>Value Name<td>Values<td>Edit<td>Description</tr>
-	<?php 
+	<tr>
+		<td colspan=4 align=center>IP tables</td>
+	</tr>
+	<tr>
+		<td>Value Name</td>
+		<td>Values</td>
+		<td>Edit</td>
+		<td>Description</td>
+	</tr>   <?php 
 		$sql1="select * from sig_ip_variables;";
 		$result1 = $conn->query($sql1);
 		if ($result1->num_rows > 0) {
 			// output data of each row
-			while($row1= $result1->fetch_assoc()) {
-	?>
+			while($row1= $result1->fetch_assoc()) { ?>
 	<tr>
-    <td><?=substr($row1["v_name"],1)?>
-	<td><?=$row1["v_value"]?>
-    <td>
-        <form method=post action="delip.php">
-		<input type="hidden" name="ipdel" value=<?=$row1["v_name"]?>>
-        <input type=submit value="삭제" >
-        </form>
-        <form method=post action="addip.php">
-        <input type="hidden" name="vname" value=<?=$row1["v_name"]?>>
-	    <input type="submit" value="수정">
-        </form>
-    </td>
-    <td> MEMO </td>
-    </tr>
+		<td><?=substr($row1["v_name"],1)?></td>
+		<td><?=$row1["v_value"]?></td>
+    		<td>
+        		<form method=post action="delip.php">
+			<input type="hidden" name="ipdel" value=<?=$row1["v_name"]?>>
+        		<input type=submit value="삭제" >
+        		</form>
+        		<form method=post action="addip.php">
+        		<input type="hidden" name="vname" value=<?=$row1["v_name"]?>>
+	    		<input type="submit" value="수정">
+        		</form>
+		</td>
+    		<td> MEMO </td>
+    	</tr>
 	<?php 
 			}
 		}
