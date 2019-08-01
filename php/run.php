6c9a497c322ceb00a2a0908a49730c41487d3a67
Lrsug <observer@localhost.localdomain>
2019-08-01 15:07:42 +0900

    sig_run ids sock

--- php/run.php
+++ php/run.php
@@ -5,13 +5,13 @@ $sid = $_GET["sig_id"];
 $chk = $_GET["chk"];
 
 //소켓 연동
-//$address = "localhost";                                             
-//$port = 5252;
-//$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP); 
-//$result = socket_connect($socket, $address, $port);
-//$i = "Sig_run sig_id=".$Rule_id.", ".$chk; 
-//socket_write($socket, $i, strlen($i)); 
-//socket_close($socket);
+$address = "localhost";                                             
+$port = 5252;
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP); 
+$result = socket_connect($socket, $address, $port);
+$i = "Sig_run sig_id=".$Rule_id.", ".$chk.";"; 
+socket_write($socket, $i, strlen($i)); 
+socket_close($socket);
 
 //sql
 $rsql="update signature set sig_run=".$chk." where sig_id=".$sid.";";
