b1f0a1d996a65e2c6b95c7018387b53baf1d0f29
Lrsug <observer@localhost.localdomain>
2019-08-01 15:29:57 +0900

    run update

--- php/run.php
+++ php/run.php
@@ -14,7 +14,7 @@ socket_write($socket, $i, strlen($i));
 socket_close($socket);
 
 //sql
-$rsql="update signature set sig_run=".$chk." where sig_id=".$sid.";";
+$rsql="update signature set sig_run=".$chk." where sig_id=".$sid;
 $conn->query($rsql);
 ?>
 <meta http-equiv="refresh" content="0,rlist.php">
