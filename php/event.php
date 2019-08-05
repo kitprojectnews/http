cfbb88bb56aff4dd23068863f70ebb1f950bc75c
jwh <ben3329@naver.com>
2019-08-05 16:53:32 +0900

    hax view

--- php/event.php
+++ php/event.php
@@ -15,21 +15,21 @@
         <table class="eventTable">
             <thead>
                 <tr>
-                    <th scope="cols" width="20px">eid</th>
-                    <th scope="cols" width="150px">time</th>
-                    <th scope="cols">sig_msg</th>
-                    <th scope="cols" width="120px">src_ip</th>
-                    <th scope="cols" width="80px">src_port</th>
-                    <th scope="cols" width="120px">dst_ip</th>
-                    <th scope="cols" width="80px">dst_port</th>
-                    <th scope="cols" width="80px">protocol</th>
+                    <th scope="cols" width="60px">eid</th>
+                    <th scope="cols" width="230px">time</th>
+                    <th scope="cols" width="500px">sig_msg</th>
+                    <th scope="cols" width="200px">src_ip</th>
+                    <th scope="cols" width="100px">src_port</th>
+                    <th scope="cols" width="200px">dst_ip</th>
+                    <th scope="cols" width="100px">dst_port</th>
+                    <th scope="cols" width="100px">protocol</th>
                     <th scope="cols">ETC</th>
                 </tr>
             </thead>
             <tbody>
                 <?php
                 include 'dbconn.php';
-                $sql = 'SELECT * FROM event_view';
+                $sql = 'SELECT * FROM event_view ORDER BY eid desc';
 
                 $result = $conn->query($sql);
                 if ($result->num_rows > 0) {
@@ -60,7 +60,9 @@
 <script launguage='JAVASCRIPT'>
     //sig_msg에 하이퍼링크 걸기, 패킷뷰에 탭넣어서 ipheader들 볼수있게 TODOTODO
     function detail(eid, sig_id) {
-        window.open('event_detailView.php?eid=' + eid + '&sig_id=' + sig_id, 'detailViewer', 'width = 1000, height = 800, menubar = no, status = no, toolbar = no ');
+        var popupX = (window.screen.width / 2) - 500;
+        var popupY= (window.screen.height / 2) - 400;
+        window.open('event_detailView.php?eid=' + eid + '&sig_id=' + sig_id, 'detailViewer', 'width = 1000, height = 800, left ='+popupX+' , top ='+popupY+', menubar = no, status = no, toolbar = no ');
     }
     //SELECT * FROM event,signature,(SELECT src_ip, dst_ip, data.eid as eid, src_port, dst_port FROM data,iphdr,tcphdr WHERE data.eid=iphdr.eid and data.eid=tcphdr.eid) AS tmp WHERE event.eid=tmp.eid
     /*
---
+++ php/event_packetview.php
@@ -0,0 +1,45 @@
+<?php
+function hexfilter($payload)
+{
+    $payload_hex = bin2hex($payload);
+    $payload_hex = strToUpper($payload_hex);
+    echo "<table align=center><tr>";
+    if(strlen($payload_hex)>0)
+        echo "<td class='td0' onMouseOver='highlightBG(\"td0\",\"rgb(119,119,119)\")' onMouseOut='highlightBG(\"td0\",\"white\")'>".$payload_hex[0];
+    for ($i = 1; $i < strlen($payload_hex); $i++) {
+        if($i%32==0)
+            echo "</tr><tr>";
+        else if($i%8==0)
+            echo "<td></td>";
+
+        if($i%2==0)
+            echo "<td class='td".($i/2)."' onMouseOver='highlightBG(\"td".($i/2)."\",\"rgb(119,119,119)\")' onMouseOut='highlightBG(\"td".($i/2)."\",\"white\")'>".$payload_hex[$i];
+        else
+            echo $payload_hex[$i]."</td>";
+    }
+    echo "</tr></table>";
+}
+function asciifilter($payload)
+{
+    echo "<table align=center><tr>";
+    for ($i = 0; $i < strlen($payload); $i++) 
+    {
+        if($i%16==0)
+            echo "</tr><tr>";
+        else if($i%4==0)
+            echo "<td></td>";
+
+        if (ord($payload[$i]) > 33 && ord($payload[$i]) < 127) 
+            echo "<td class='td".$i."' onMouseOver='highlightBG(\"td".$i."\",\"rgb(119,119,119)\")' onMouseOut='highlightBG(\"td".$i."\",\"white\")'>".$payload[$i]."</td>";
+        else 
+            echo "<td class='td".$i."' onMouseOver='highlightBG(\"td".$i."\",\"rgb(119,119,119)\")' onMouseOut='highlightBG(\"td".$i."\",\"white\")'>.</td>";
+    }
+    echo "</tr></table>";
+}
+function viewtext($payload)
+{
+    echo "<p align='left'>";
+    echo $payload;
+    echo "</p>";
+}
+?>
