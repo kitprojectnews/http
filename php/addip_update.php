24cf39554d01ef6bbef508e5d4d1a495ba59b81b
StarRate1214 <49301117+StarRate1214@users.noreply.github.com>
2019-08-30 20:26:35 +0900

    Update addip_update.php

--- php/addip_update.php
+++ php/addip_update.php
@@ -2,14 +2,12 @@
 session_start();
 if(!$_SESSION['u_active'])//활성 유저 여부 
 {
-    echo " <script>alert('비활성화된 사용자입니다. '); history.back(); </script>";
+ header('Location:../index.html');
 }
-
 if(!$_SESSION['r_update'])//룰 추가 수정 삭제 가능 여부
 {
-   echo " <script>alert('접근권한이 없습니다. '); history.back(); </script>";
+   die('접근권한이 없습니다. ');
 }
-
     include 'dbconn.php';
     //ip업데이트
 
