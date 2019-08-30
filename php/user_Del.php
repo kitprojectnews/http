5f40862b670949c104d822a75f287b2bfd8c50c0
StarRate1214 <49301117+StarRate1214@users.noreply.github.com>
2019-08-30 20:45:17 +0900

    Update user_Del.php

--- php/user_Del.php
+++ php/user_Del.php
@@ -8,11 +8,11 @@
 session_start();
 if(!$_SESSION['u_active'])//활성 유저 여부 
 {
-    echo " <script>alert('비활성화된 사용자입니다. '); history.back(); </script>";
+ header('Location:../index.html');
 }
 if(!$_SESSION['u_update'])//유저 관리권한 여부
 {
-    echo " <script>alert('접근권한이 없습니다. '); history.back(); </script>";
+   die('접근권한이 없습니다. ');
 }
 include "dbconn.php";
 if (isset($_GET["u_num"])) {
