63f59f78112768164d803897c47abb1fc0dd983e
StarRate1214 <49301117+StarRate1214@users.noreply.github.com>
2019-08-30 20:12:16 +0900

    Update addip.php

--- php/addip.php
+++ php/addip.php
@@ -2,6 +2,17 @@
 <title>Variable</title>
 
 <?php
+session_start();
+if(!$_SESSION['u_active'])//활성 유저 여부 
+{
+    echo " <script>alert('비활성화된 사용자입니다. '); history.back(); </script>";
+}
+
+if(!$_SESSION['r_update'])//룰 추가 수정 삭제 가능 여부
+{
+   echo " <script>alert('접근권한이 없습니다. '); history.back(); </script>";
+}
+
     include 'dbconn.php';
     
     $vname = $_POST["vname"];
