046bfa1538e99b7093877623d80713fe342a73ec
StarRate1214 <49301117+StarRate1214@users.noreply.github.com>
2019-08-30 20:50:49 +0900

    Update login_ok.php

--- php/login_ok.php
+++ php/login_ok.php
@@ -8,23 +8,6 @@ include "dbconn.php";
 
 //How to use - 세션 확인 + 활성 유저 여부 + 각각 필요한 권한
 session_start();
-// if (!isset($_SESSION['user_num']))//세션 확인
-// {
-//     header('Location:../index.html');
-// }
-// if(!$_SESSION['u_active'])//활성 유저 여부 
-// {
-//     echo " <script>alert('비활성화된 사용자입니다. '); history.back(); </script>";
-// }
-// if(!$_SESSION['u_update'])//유저 관리권한 여부
-// {
-//     echo " <script>alert('접근권한이 없습니다. '); history.back(); </script>";
-// }
-// if(!$_SESSION['r_update'])//룰 추가 수정 삭제 가능 여부
-// {
-//     echo " <script>alert('접근권한이 없습니다. '); history.back(); </script>";
-// }
-
 $user = $_POST["user"];
 $pass = $_POST["pass"];
 
