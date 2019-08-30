0367ebbdee6288e0ee8707802358a02acc73a4aa
StarRate1214 <49301117+StarRate1214@users.noreply.github.com>
2019-08-30 20:28:21 +0900

    Update event.php

--- php/event.php
+++ php/event.php
@@ -1,4 +1,15 @@
 <html>
+<?php
+session_start();
+if(!$_SESSION['u_active'])//활성 유저 여부 
+{
+ header('Location:../index.html');
+}
+if(!$_SESSION['r_update'])//룰 추가 수정 삭제 가능 여부
+{
+   die('접근권한이 없습니다. ');
+}
+?>
 
 <head>
     <link rel="stylesheet" type="text/css" href="../css/Observer_event.css" />
