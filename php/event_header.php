32c2c2dccd612270d62870a14c182892187a0c02
StarRate1214 <49301117+StarRate1214@users.noreply.github.com>
2019-08-30 20:30:40 +0900

    Update event_header.php

--- php/event_header.php
+++ php/event_header.php
@@ -1,3 +1,14 @@
+<?php
+session_start();
+if(!$_SESSION['u_active'])//활성 유저 여부 
+{
+ header('Location:../index.html');
+}
+if(!$_SESSION['r_update'])//룰 추가 수정 삭제 가능 여부
+{
+   die('접근권한이 없습니다. ');
+}
+?>
 <html>
 <head>
     <link rel="stylesheet" type="text/css" href="../css/Observer_eventHeader.css" />
