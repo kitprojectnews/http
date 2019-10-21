0cea89c0ab374a3a67ef3d0ca4a82d08215f8901
lrsug628 <49192741+lrsug628@users.noreply.github.com>
2019-10-21 20:49:18 +0900

    Update event_alert_server.php

--- php/event_alert_server.php
+++ php/event_alert_server.php
@@ -1,6 +1,19 @@
 <?php
 include "dbconn.php";
 
+session_start();
+if($_SESSION['eid']==-1)
+{
+    $sql = "SELECT eid FROM alert_view ORDER BY eid DESC limit 1";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) 
+    {
+        while ($row = $result->fetch_assoc()) 
+        {
+            $_SESSION['eid'] = $row["eid"]+1;
+        }
+    }
+}
  $sql = "SELECT * FROM alert_view WHERE eid >= '".$_SESSION['eid']."' order by eid desc limit 1";
  $result = $conn->query($sql);
  if ($result->num_rows > 0) {
