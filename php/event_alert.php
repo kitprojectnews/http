afa7d1b73a9d552ebff51cc6d62d9d856e1ed337
jwh <jwh@localhost.localdomain>
2019-10-24 19:11:45 +0900

    [+] alarm window open function add

--- php/event_alert.php
+++ php/event_alert.php
@@ -5,6 +5,12 @@ $_SESSION['eid'] = -1;
 $_SESSION['eidset'] = -1;
 ?>
 <link rel="stylesheet" type="text/css" href="../css/Observer_alert.css" />
+<style type="text/css">
+    a:link { color: black; text-decoration: none;}
+    a:visited { color: black; text-decoration: none;}
+    a:hover { color: black; text-decoration: none;}
+</style>
+
 
 <form id=div_alertcheckbox>
 </form>
@@ -22,4 +28,12 @@ $_SESSION['eidset'] = -1;
         xhttp.open("GET", "event_alert_server.php", true);
         xhttp.send();
     }, 1000);
+</script>
+<script launguage='JAVASCRIPT'>
+    //sig_msg에 하이퍼링크 걸기, 패킷뷰에 탭넣어서 ipheader들 볼수있게 TODOTODO
+    function detail(eid, sig_id) {
+        var popupX = (window.screen.width / 2) - 500;
+        var popupY= (window.screen.height / 2) - 400;
+        window.open('event_detailView.php?eid=' + eid + '&sig_id=' + sig_id, 'detailViewer', 'width = 1000, height = 800, left ='+popupX+' , top ='+popupY+', menubar = no, status = no, toolbar = no ');
+    }
 </script>
--- php/event_alert_server.php
+++ php/event_alert_server.php
@@ -14,7 +14,8 @@ if($_SESSION['eid']==-1)
         }
     }
 }
- $sql = "SELECT * FROM alert_view WHERE eid >= '".$_SESSION['eid']."' order by eid desc limit 1";
+ $sql = "SELECT *,event_view.sig_id FROM alert_view INNER JOIN event_view ON alert_view.eid = '".$_SESSION['eid']."' order by alert_view.eid desc limit 1";
+ //$sql = "SELECT * FROM alert_view WHERE eid >= '".$_SESSION['eid']."' order by eid desc limit 1";
  $result = $conn->query($sql);
  if ($result->num_rows > 0) {
     echo "<table align=center border=0 width=100%  style='border-collapse: collapse;'>";
@@ -32,23 +33,22 @@ if($_SESSION['eid']==-1)
                 switch($row["severity"]){
                     case 1:
                     echo("<tr  id='NA'>");
-                    echo("<td align=center width=90%>Rule [ ".$msg." ] is Matched.</td><td align=center width=10%><span class='css-cancel' onclick='javascript:location.reload(true)'></span></td></tr>");
-                        break;
+                    echo("<td align=center width=90%><a href=javascript:detail(".$row["eid"].",".$row["sig_id"].");>Rule [ ".$msg." ] is Matched.</a></td><td align=center width=10%><span class='css-cancel' onclick='javascript:location.reload(true)'></span></td></tr>");                        break;
                     case 2:
                     echo("<tr  id='Low'>");
-                    echo("<td align=center width=90%>Rule [ ".$msg." ] is Matched.</td><td align=center width=10%><span class='css-cancely' onclick='javascript:location.reload(true)'></span></td></tr>");
+                    echo("<td align=center width=90%><a href=javascript:detail(".$row["eid"].",".$row["sig_id"].");>Rule [ ".$msg." ] is Matched.</a></td><td align=center width=10%><span class='css-cancel' onclick='javascript:location.reload(true)'></span></td></tr>");                        break;
                         break;
                     case 3:
                     echo("<tr  id='Medium'>");
-                    echo("<td align=center width=90%>Rule [ ".$msg." ] is Matched.</td><td align=center width=10%><span class='css-cancel' onclick='javascript:location.reload(true)'></span></td></tr>");
+                    echo("<td align=center width=90%><a href=javascript:detail(".$row["eid"].",".$row["sig_id"].");>Rule [ ".$msg." ] is Matched.</a></td><td align=center width=10%><span class='css-cancel' onclick='javascript:location.reload(true)'></span></td></tr>");                        break;
                         break;
                     case 4:
                     echo("<tr  id='High'>");
-                    echo("<td align=center width=90%>Rule [ ".$msg." ] is Matched.</td><td align=center width=10%><span class='css-cancel' onclick='javascript:location.reload(true)'></span></td></tr>");
+                    echo("<td align=center width=90%><a href=javascript:detail(".$row["eid"].",".$row["sig_id"].");>Rule [ ".$msg." ] is Matched.</a></td><td align=center width=10%><span class='css-cancel' onclick='javascript:location.reload(true)'></span></td></tr>");                        break;
                         break;
                     case 5:
                     echo("<tr  id='Critical'>");
-                    echo("<td align=center width=90%>Rule [ ".$msg." ] is Matched.</td><td align=center width=10%><span class='css-cancel' onclick='javascript:location.reload(true)'></span></td></tr>");
+                    echo("<td align=center width=90%><a href=javascript:detail(".$row["eid"].",".$row["sig_id"].");>Rule [ ".$msg." ] is Matched.</a></td><td align=center width=10%><span class='css-cancel' onclick='javascript:location.reload(true)'></span></td></tr>");                        break;
                         break;
                 }
             
@@ -60,3 +60,11 @@ if($_SESSION['eid']==-1)
  else {
  }
 ?>
+<script launguage='JAVASCRIPT'>
+    //sig_msg에 하이퍼링크 걸기, 패킷뷰에 탭넣어서 ipheader들 볼수있게 TODOTODO
+    function detail(eid, sig_id) {
+        var popupX = (window.screen.width / 2) - 500;
+        var popupY= (window.screen.height / 2) - 400;
+        window.open('event_detailView.php?eid=' + eid + '&sig_id=' + sig_id, 'detailViewer', 'width = 1000, height = 800, left ='+popupX+' , top ='+popupY+', menubar = no, status = no, toolbar = no ');
+    }
+</script>
